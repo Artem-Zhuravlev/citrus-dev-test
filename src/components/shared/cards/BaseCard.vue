ec3ac8b476e79ed10bc9e50bed2f3741116b8847
Artem <mstrjesm@gmail.com>
2024-10-17 20:33:36 +0300

    feat(base-card): update styles

--- src/components/shared/cards/BaseCard.vue
+++ src/components/shared/cards/BaseCard.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="card">
     <h3 class="card-title">{{ title }}</h3>
-    <p>{{ description }}</p>
+    <p class="card-description">{{ description }}</p>
   </article>
 </template>
 
@@ -14,12 +14,16 @@ const props = defineProps({
 
 <style>
 .card {
-  @apply bg-white border border-gray-200 rounded-lg shadow-md p-6;
+  @apply bg-white p-6 rounded-3xl shadow-md shadow-gray-900/5;
 }
 .card-title {
-  @apply text-lg font-bold mb-4;
+  @apply mt-4 mb-4 text-xl font-semibold leading-6;
 }
 .card-button {
   @apply bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4;
+}
+
+.card-description {
+  @apply text-3xl text-cyan-500;
 }
 </style>
