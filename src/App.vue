80237c319113911d95c410d6c821d6a86637c792
Artem <mstrjesm@gmail.com>
2024-10-17 19:20:58 +0300

    feat: add styles for container

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 </script>
 
 <template>
-
+  <div class="container">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, omnis sint inventore laudantium, alias, assumenda totam quo voluptates laboriosam facere autem beatae? Quos doloremque nisi sint exercitationem fugit. Maiores, deleniti.</p>
+  </div>
 </template>
 
 <style scoped>
