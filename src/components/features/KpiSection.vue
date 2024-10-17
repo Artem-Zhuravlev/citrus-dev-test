d96667c23af16316b41bd0207e774f2ac8e2dab9
Artem <mstrjesm@gmail.com>
2024-10-17 20:38:11 +0300

    feat(kpi-section): add component

---
+++ src/components/features/KpiSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="section grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <base-card
+      v-for="(item, index) in items"
+      :key="index"
+      :title="item.title"
+      :description="item.value.toString()"
+    />
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import { BaseCard } from '@components/shared';
+
+const props = defineProps({
+  items: {
+    type: Array,
+    default: () => []
+  }
+});
+</script>
