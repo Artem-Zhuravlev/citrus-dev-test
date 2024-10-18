052afb49eb5e243210c6e259b3eeae9c056d414f
Artem <mstrjesm@gmail.com>
2024-10-18 21:19:54 +0300

    feat: add kpi translations

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,16 +1,19 @@
 import { createI18n } from 'vue-i18n';
 import { regions } from './regions';
+import { kpi } from './kpi';
 
 export const i18n = createI18n({
   locale: 'en',
   fallbackLocale: 'ua',
   messages: {
     en: {
-      regions: { ...regions.en }
+      regions: { ...regions.en },
+      kpi: { ...kpi.en }
 
     },
     ua: {
-      regions: { ...regions.ua }
+      regions: { ...regions.ua },
+      kpi: { ...kpi.ua }
     }
   }
 });
