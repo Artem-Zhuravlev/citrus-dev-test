d533cc30eef574131b0745a96edf8ff945620f21
Artem <mstrjesm@gmail.com>
2024-10-18 19:37:03 +0300

    feat(table-base): add to public api

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -1,3 +1,4 @@
 export * from './cards';
 export * from './charts';
 export * from './inputs';
+export * from './tables';
---
+++ src/components/shared/tables/index.js
@@ -0,0 +1,5 @@
+import TableBase from "./TableBase.vue";
+
+export {
+  TableBase
+}
