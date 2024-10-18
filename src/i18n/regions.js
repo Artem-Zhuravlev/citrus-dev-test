5c13eaf30a57624233cce5e416d099d62c6e4cac
Artem <mstrjesm@gmail.com>
2024-10-18 20:36:32 +0300

    feat: add translations for regions

---
+++ src/i18n/regions.js
@@ -0,0 +1,14 @@
+export const regions = {
+  en: {
+    north_america: 'North America',
+    europe: 'Europe',
+    asia: 'Asia',
+    south_america: 'South America'
+  },
+  ua: {
+    north_america: 'Північна Америка',
+    europe: 'Європа',
+    asia: 'Азія',
+    south_america: 'Південна Америка'
+  }
+}
