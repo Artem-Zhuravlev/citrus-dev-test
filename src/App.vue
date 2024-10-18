b3046aaff9b78d4bddaaa1a82415c461cf2cf689
Artem <mstrjesm@gmail.com>
2024-10-18 22:54:25 +0300

    refactor(app): remove unused data

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,42 @@
 <template>
   <div class="container">
-    <KpiSection :items="kpiCards" />
+    <KpiSection />
     <div class="section">
       <InputSelect :options="selectOptions" v-model="selectedOption"/>
-      <InputSelect :options="countries" v-model="selectedOption"/>
+      <InputSelect :options="salesStore.countries" v-model="selectedOption"/>
     </div>
-		{{ totalSales }} {{  numberOfOrders }}
+
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-      <LineChart :data="lineChartData"/>
-      <BarChart :data="lineChartData"/>
+      <LineChart :data="chartData"/>
+      <BarChart :data="chartData"/>
     </div>
 		<div class="section">
-			<TableBase :headers="headers" :items="items"/>
+			<TableBase :headers="headers" :items="tableData"/>
 		</div>
   </div>
 </template>
 
 <script setup>
-import { computed, reactive, ref } from 'vue';
+import { ref } from 'vue';
 import { KpiSection } from '@/components/features';
 import { LineChart, BarChart, TableBase } from '@/components/shared';
 import { InputSelect } from '@/components/shared';
-import { salesMockData } from '@/mocks';
+import { useSalesStore } from '@/stores/sales';
 import { useI18n } from 'vue-i18n';
 
-const { t } = useI18n();
-const salesData = reactive(salesMockData);
+const salesStore = useSalesStore();
+const { chartData, tableData } = salesStore;
 
-const countries = computed(() => {
-	const uniqueRegions = new Set(salesData.map((item) => item.region));
-
-	return Array.from(uniqueRegions).map((region) => {
-		return {
-			value: region,
-			label: t(`regions.${region}`),
-		};
-	});
-});
-
-const totalSales = computed(() => {
-	return salesData.reduce((acc, item) => acc + item.quantity_sold, 0);
-});
-
-const numberOfOrders = computed(() => salesData.length);
-
-const kpiCards = reactive([
-	{ title: 'Total sales', value: 42 },
-	{ title: 'Number of orders', value: 20 },
-	{ title: 'Top-performing product', value: 10 },
-	{ title: 'Sales growth percentage', value: 15 },
-]);
 const selectOptions = [
 	{ value: 'lastWeek', label: 'Last week' },
 	{ value: 'lastMonth', label: 'Last month' },
 ];
 const selectedOption = ref();
 
-const lineChartData = reactive([
-	{
-		label: 'Some label',
-		list: [
-			{
-				value: 10,
-				label: 'January',
-			},
-			{
-				value: 16,
-				label: 'February',
-			},
-			{
-				value: 20,
-				label: 'March',
-			},
-			{
-				value: 25,
-				label: 'April',
-			},
-			{
-				value: 35,
-				label: 'May',
-			},
-		],
-	},
-]);
-
 const headers = [
-	{ text: 'Name', value: 'name' },
-	{ text: 'Height (cm)', value: 'height', sortable: true },
-	{ text: 'Weight (kg)', value: 'weight', sortable: true },
-	{ text: 'Age', value: 'age', sortable: true },
-];
-
-const items = [
-	{ name: 'Curry', height: 178, weight: 77, age: 20 },
-	{ name: 'James', height: 180, weight: 75, age: 21 },
-	{ name: 'Jordan', height: 181, weight: 73, age: 22 },
+	{ text: 'Order ID', value: 'id' },
+	{ text: 'Product name', value: 'product_name', sortable: true },
+	{ text: 'Total Price', value: 'total_price', sortable: true },
+	{ text: 'Region', value: 'region', sortable: true },
 ];
 </script>
