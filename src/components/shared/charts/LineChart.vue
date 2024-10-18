bf9ada51499873cf90811210a88e8784134be6d9
Artem <mstrjesm@gmail.com>
2024-10-18 19:38:08 +0300

    refactor(line-chart): code format

--- src/components/shared/charts/LineChart.vue
+++ src/components/shared/charts/LineChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="chart-wrapper">
+  <div class="cart-wrapper">
     <Line :data="chartData" :options="options" />
   </div>
 </template>
@@ -8,51 +8,51 @@
 import { computed, reactive } from 'vue';
 import options from './chartOptions';
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-} from 'chart.js'
-import { Line } from 'vue-chartjs'
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+} from 'chart.js';
+import { Line } from 'vue-chartjs';
 
 ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
 );
 
 const props = defineProps({
-  data: {
-    type: Array,
-    default: () => []
-  }
+	data: {
+		type: Array,
+		default: () => [],
+	},
 });
 
 const labels = computed(() => {
-  if (props.data.length > 0) {
-    return props.data[0].list.map(chart => chart.label);
-  }
-  return [];
+	if (props.data.length > 0) {
+		return props.data[0].list.map((chart) => chart.label);
+	}
+	return [];
 });
 
 const datasets = computed(() => {
-  return props.data.map(chart => {
-    return {
-      label: chart.label,
-      borderColor: '#06b6d4',
-      data: chart.list.map(item => item.value),
-    }
-  });
+	return props.data.map((chart) => {
+		return {
+			label: chart.label,
+			borderColor: '#06b6d4',
+			data: chart.list.map((item) => item.value),
+		};
+	});
 });
 
 const chartData = computed(() => ({
-  labels: labels.value,
-  datasets: datasets.value
+	labels: labels.value,
+	datasets: datasets.value,
 }));
 </script>
