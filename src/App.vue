7da45570de1b51b58f13379280f54134a81425ec
Artem <mstrjesm@gmail.com>
2024-10-18 14:29:47 +0300

    feat(app): add input select and mock data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
     <KpiSection :items="kpiCards" />
+    <div class="section">
+      <InputSelect :options="selectOptions" v-model="selectedOption"/>
+    </div>
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
       <LineChart :data="lineChartData"/>
       <BarChart :data="lineChartData"/>
@@ -9,9 +12,10 @@
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import { KpiSection } from '@/components/features';
 import { LineChart, BarChart } from '@/components/shared';
+import { InputSelect } from '@/components/shared';
 
 const kpiCards = reactive([
   { title: 'Total sales', value: 42 },
@@ -19,6 +23,12 @@ const kpiCards = reactive([
   { title: 'Top-performing product', value: 10 },
   { title: 'Sales growth percentage', value: 15 },
 ]);
+
+const selectedOption = ref(null);
+const selectOptions = [
+  { value: 'lastWeek', label: 'Last week' },
+  { value: 'lastMonth', label: 'Last month' },
+];
 
 const lineChartData = reactive([
   {
