411fa76cd3e7a2ebea26eb87a47d07f2883d9d0b
Artem <mstrjesm@gmail.com>
2024-10-18 22:53:46 +0300

    feat(kpi-section): add render for kpis

--- src/components/features/KpiSection.vue
+++ src/components/features/KpiSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="section grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
     <base-card
-      v-for="(item, index) in items"
+      v-for="(item, index) in kpiCards"
       :key="index"
       :title="item.title"
       :description="item.value.toString()"
@@ -10,13 +10,20 @@
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
+import { reactive } from 'vue';
 import { BaseCard } from '@/components/shared';
+import { useSalesStore } from '@/stores/sales';
+import { useI18n } from 'vue-i18n';
 
-const props = defineProps({
-  items: {
-    type: Array,
-    default: () => []
-  }
-});
+const salesStore = useSalesStore();
+const { t } = useI18n();
+
+const { totalSales, numberOfOrders, topPerformingProduct } = salesStore;
+
+const kpiCards = reactive([
+	{ title: t('kpi.total_sales'), value: totalSales },
+	{ title: t('kpi.number_of_orders'), value: numberOfOrders },
+	{ title: t('kpi.top_performing_product'), value: topPerformingProduct },
+	{ title: t('kpi.sales_growth_percentage'), value: 15 },
+]);
 </script>
