b8c9281e536c9653812b9f3422dc690c84a56660
Artem <mstrjesm@gmail.com>
2024-10-18 21:19:26 +0300

    feat: add kpi translations

---
+++ src/i18n/kpi.js
@@ -0,0 +1,14 @@
+export const kpi = {
+  en: {
+    total_sales: 'Total sales',
+    number_of_orders: 'Number of orders',
+    top_performing_product: 'Top-performing product',
+    sales_growth_percentage: 'Sales growth percentage'
+  },
+  ua: {
+    total_sales: 'Загальні продажі',
+    number_of_orders: 'Кількість замовлень',
+    top_performing_product: 'Найуспішніший продукт',
+    sales_growth_percentage: 'Відсоток зростання продажів'
+  }
+}
